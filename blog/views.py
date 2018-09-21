d3d851d27ac329b2ebca454d3ea95036f6f0e9cb
Frank Liu <python.frankliu@gmail.com>
2018-09-21 10:30:14 -0500

    finsih 10

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 
@@ -11,13 +12,42 @@ def home(request):
 
 
 class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url='/'
+
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
-# from django.views.generic import ListView
-# class PostListView(ListView):
-#     model = Post
-#     context_object_name = 'posts'
-#     template_name = 'blog/home.html'
